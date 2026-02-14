8b25e2da2f754f2cf539d5940aea32374e69d264
firas1996 <firas.hm.1996@gmail.com>
2026-02-14 10:09:12 +0100

    aa

--- models/userModel.js
+++ models/userModel.js
@@ -13,6 +13,36 @@ const userSchema = mongoose.Schema({
     validate: [validator.isEmail, "The email is not valid !!!"],
     lowercase: true,
   },
+  password: {
+    type: String,
+    required: [true, "The password is required !!!"],
+    minlength: 8,
+    // validate: validator.isStrongPassword,
+  },
+  confirm_password: {
+    type: String,
+    required: [true, "The password is required !!!"],
+    minlength: 8,
+    validate: {
+      validator: (cPass) => {
+        return cPass === this.password;
+      },
+      message: "Pass and cPass does not match !!!",
+    },
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  pass_changed_at: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const User = mongoose.model("User", userSchema);
